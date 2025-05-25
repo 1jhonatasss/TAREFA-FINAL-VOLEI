369ef8a070b03821ba80598633b7feb99d9f669b
Jhonatas <Jhonatas_arj@hotmail.com>
2025-05-25 16:48:10 -0300

    concluido

--- index.js
+++ index.js
@@ -13,24 +13,28 @@ app.get("/", (req, res) => {
         <html lang="pt-br">
             <head>
                 <meta charset="UTF-8">
+                <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
                 <title>Página Inicial</title>
             </head>
             <body>
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">CLÍNICA DE SAÚDE MENTAL</a>
+                        <a class="navbar-brand position-absolute start-50 translate-middle-x" style="color:rgb(41, 184, 219)"; href="/">CLÍNICA DE SAÚDE MENTAL</a>
                         <div class="collapse navbar-collapse">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item"><a class="nav-link active" href="/">Início</a></li>
+                                <li class="nav-item"><a class="nav-link active" href="/login">ACESSAR CONTA</a></li> 
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Agendamento</a>
+                                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">AGENDAMENTOS</a>
                                     <ul class="dropdown-menu">
                                         <li><a class="dropdown-item" href="/cadastro">Agendar consulta</a></li>
                                         <li><a class="dropdown-item" href="/agenda">Conferir agenda</a></li>
                                     </ul>
                                 </li>
                             </ul>
+                                <div class="ms-auto me-4">
+                                    <a class="nav-link active fs-5" style="font-family: 'Dancing Script', cursive;">Dra.ACândido</a>
+                                </div>
                         </div>
                     </div>
                 </nav>
@@ -143,6 +147,88 @@ app.post("/cadastro", (req, res) => {
             </body>
         </html>
     `);
+});
+
+app.get("/login",(req, res)=>{
+    res.send(`
+
+                            <style>
+                            .wrapper {    
+                        margin-top: 80px;
+                        margin-bottom: 20px;
+                    }
+
+                    .form-signin {
+                    max-width: 420px;
+                    padding: 30px 38px 66px;
+                    margin: 0 auto;
+                    background-color: #eee;
+                    border: 3px dotted rgba(0,0,0,0.1);  
+                    }
+
+                    .form-signin-heading {
+                    text-align:center;
+                    margin-bottom: 30px;
+                    }
+
+                    .form-control {
+                    position: relative;
+                    font-size: 16px;
+                    height: auto;
+                    padding: 10px;
+                    }
+
+                    input[type="text"] {
+                    margin-bottom: 0px;
+                    border-bottom-left-radius: 0;
+                    border-bottom-right-radius: 0;
+                    }
+
+                    input[type="senha"] {
+                    margin-bottom: 20px;
+                    border-top-left-radius: 0;
+                    border-top-right-radius: 0;
+                    }
+
+                    .colorgraph {
+                    height: 7px;
+                    border-top: 0;
+                    background: #c4e17f;
+                    border-radius: 5px;
+                    background-image: -webkit-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+                    background-image: -moz-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+                    background-image: -o-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+                    background-image: linear-gradient(to right, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+                    }
+                    </style>
+                                            <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+                        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+                        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+                        <!------ Include the above in your HEAD tag ---------->
+
+                        <div class = "container">
+                            <div class="wrapper">
+                                <form action="" method="post" name="Login_Form" class="form-signin">       
+                                    <h3 class="form-signin-heading">Welcome Back! Please Sign In</h3>
+                                    <hr class="colorgraph"><br>
+                                    
+                                    <input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" />
+                                    <input type="senha" class="form-control" name="Senha" placeholder="Senha" required=""/>     		  
+                                    
+                                    <button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>  			
+                                </form>			
+                            </div>
+                        </div>
+                    
+        
+        `);
+});
+
+app.post("/login", (req,res) => {
+    const usuario = req.body.usuario;
+    const senha = req.body.senha;
+
+    res.redirect("/");
 });
 
 app.get("/agenda", (req, res) => {
